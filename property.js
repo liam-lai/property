00041fc7332b1285f98ddc3c5f62325207ea7731
liam-lai <ic09272002@yahoo.com.tw>
2021-01-13 00:19:28 +1100

    update tax

--- property.js
+++ property.js
@@ -1,5 +1,5 @@
 const PropertyValue = 800000;
-const LVM = 0.8; //you borrow 80%
+let LVM = 0.8; //you borrow 80%
 const InterestOnlyRate = 0.0329; // https://www.commbank.com.au/home-loans.html
 
 const CashIn = {
@@ -47,18 +47,22 @@ const TotalTaxLost = () => {
 
 const destiny = () => {
     let cash = TotalCashIn() - TotalCashOut();
-    const tax = (TotalCashIn() - TotalTaxLost()) * 0.37;
-    cash += tax;
+    const tax = (TotalCashIn() - TotalTaxLost()) * 0.37; 
+    cash -= tax; // if tax is negative, then we can money from ATO
 
     const InitialPrincipal = PropertyValue * (1 - LVM);
     if (tax > 0) {
-        console.log("yearly tax pay: ", tax.toFixed(1));
+        //console.log("yearly tax pay: ", tax.toFixed(1));
     } else {
-        console.log("yearly tax return: ", tax.toFixed(1) * -1);
+        //console.log("yearly tax return: ", tax.toFixed(1) * -1);
     }
 
-    console.log("yearly total earn: ", cash.toFixed(1));
-    console.log("yearly return rate", (cash / InitialPrincipal).toFixed(4));
+    //console.log("yearly total earn: ", cash.toFixed(1));
+    console.log("yearly return rate", (cash / InitialPrincipal).toFixed(4), ", lvm: ", LVM.toFixed(3));
 };
 
-destiny();
+for (let i = 0.9; i >= 0.2; i-=0.1) {
+    LVM = i
+    destiny();
+}
+
